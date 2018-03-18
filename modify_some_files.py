c9f03566f73e8e6affc3012e4d26d555c3b0dc3f
mattbellis <matthew.bellis@gmail.com>
2018-03-18 03:35:47 -0400

    Got it all working I think.

--- modify_some_files.py
+++ modify_some_files.py
@@ -6,16 +6,54 @@ import numpy as np
 
 infilename = sys.argv[1]
 
+meta = []
+tmpinfile = open(infilename,'r')
+save_line = False
+output = ""
+for line in tmpinfile:
+    if '<mgrwt>' in line:
+        output = ""
+        save_line = True
+    elif '</mgrwt>' in line:
+        output += line
+        save_line = False
+        meta.append(output)
+        output = ""
+
+    if save_line:
+        output += line
+
+
+
+
+
+
+outfilename = "testout.lhe"
+
 header = mlt.get_header(infilename)
 footer = '</LesHouchesEvents>'
 
 print(header)
 print(footer)
 
+outfile = open(outfilename,"w")
+outfile.write(header)
+
 lhfile = pylhef.read(infilename)
 
-for event in lhfile.events:
-    print("------------")
+output = ""
+for mginfo,event in zip(meta,lhfile.events):
+    #print("------------")
+    output = "<event>\n"
+
+    #print(event.nup,event.process_id,event.weight,event.scale,event.alpha_qcd,event.alpha_qed)
+    #output += "%+.10e " % (evnent.nup)
+    output += "%-7d " % (6)
+    output += "%d " % (event.process_id)
+    output += "%+.7e " % (event.weight)
+    output += "%.8e " % (event.scale)
+    output += "%.8e " % (event.alpha_qcd)
+    output += "%.8e\n" % (event.alpha_qed)
 
     particles = event.particles
 
@@ -24,15 +62,15 @@ for event in lhfile.events:
     for ipart,particle in enumerate(particles):
 
         if particle.id != 6:
-            output = mlt.write_particle(particle)
-            print(output)
+            output += mlt.write_particle(particle)
+            #print(output)
 
         elif particle.id == 6: # top quark
 
             particle.status = 2 # BECAUSE IT DECAYED IN LHE IS THIS TRUE???
 
-            output = mlt.write_particle(particle,status=2)
-            print(output)
+            output += mlt.write_particle(particle,status=2)
+            #print(output)
 
             energy = particle.p[0]
             px = particle.p[1]
@@ -83,8 +121,17 @@ for event in lhfile.events:
     for nc in new_children:
         p = nc[0]
         ipart = nc[1]
-        output = mlt.write_particle(p,parent_index=ipart)
-        print(output)
+        output += mlt.write_particle(p,parent_index=ipart)
+        #print(output)
 
+    output += mginfo
+    output += "</event>\n"
+    print(output)
+    outfile.write(output)
+
+
+outfile.write(footer)
+outfile.write("\n")
+outfile.close()
 
 
